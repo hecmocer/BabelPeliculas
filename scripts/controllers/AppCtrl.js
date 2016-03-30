a23fc8ee616f0ea64f1beb5f8fad7283d0f01f97
Jorge González <jorge.gonzalez@babel.es>
2016-03-30 13:36:09 +0200

    Navbar completa con sincronización del título de la web y la pestaña seleccionada.

---
+++ scripts/controllers/AppCtrl.js
@@ -0,0 +1,24 @@
+angular.module("moviedb").controller("AppCtrl", ["$scope", "$location", "paths", function($scope, $location, paths) {
+	var controller = this;
+
+	// controller properties
+	controller.titles = {};
+	controller.titles[paths.home] = "Home";
+	controller.titles[paths.movies] = "Movies";
+	controller.titles[paths.newMovie] = "Upload";	
+
+    // model init
+    $scope.model = { // es buena practica tener un objeto para el modelo
+        title: ""
+    };
+
+    // scope event listeners
+    $scope.$on("$locationChangeSuccess", function (evt, currentRoute) {
+    	$scope.model.title = controller.titles[$location.path()] || "404 Not Found";
+    	console.log("TITULO",$scope.model.title);
+    });
+
+    $scope.$on("$changeTitle", function (evt, title) {
+        $scope.model.title = title;
+    })
+}]);
