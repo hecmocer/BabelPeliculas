95937b44236c58e1b9d17e617200c0906e9ddefb
Héctor <hecmocer@gmail.com>
2016-03-30 11:51:10 +0200

    Quitados comentarios en app.js

--- scripts/app.js
+++ scripts/app.js
@@ -18,24 +18,3 @@ angular.module("moviedb", ["ngRoute"]).config(
         }).otherwise({
             templateUrl: "views/404.html"
         });
-
-
-
-
-// }).when(paths.newMovie, {
-//     templateUrl: 'views/NewMovie.html'
-// }).when(paths.movieDetail, {
-//     templateUrl: 'views/MediaItemDetail.html',
-//     controller: 'MovieDetailController'
-// }).when(paths.series, {
-//     templateUrl: 'views/SeriesList.html'
-// }).when(paths.serieDetail, {
-//     templateUrl: 'views/MediaItemDetail.html',
-//     controller: 'SerieDetailController'
-// }).when(paths.people, {
-//     templateUrl: 'views/PeopleList.html'
-// }).when(paths.home, {
-//     redirectTo: paths.movies
-// }).otherwise({
-//     templateUrl: 'views/404.html'
-// })    }]);
